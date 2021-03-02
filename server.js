5c91ec71e13dc9a23384f81e8cbffb74f3103958
David_Velez <velezdavid6@gmail.com>
2021-03-02 15:51:10 -0500

    finished auth logic

--- server.js
+++ server.js
@@ -1,15 +1,25 @@
+require('dotenv').config()
 const express = require('express')
-const app = express()
+const connectDB = require('./config/db')
+const errorHandler = require('./middleware/error')
 
+connectDB()
 
-app.get('/', (req, res) => {
-    res.send('Hello World')
-})
+const app = express();
 
-// Initialize the server
+app.use(express.json())
+
+app.use('/api/auth', require('./routes/auth'))
+app.use('/api/private', require('./routes/private'))
+
+// Error handler should be last piece of middleware
+app.use(errorHandler)
+
+// Initialize server
 
 const PORT = process.env.PORT || 5000
-const server = app.listen(PORT, console.log(`Server started on port ${PORT}`))
+
+const server = app.listen(PORT, console.log(`Server running on port ${PORT}`));
 process.on('unhandledRejection', (err, promise) => {
     console.log(`Logged Error: ${err}`)
     server.close(() => process.exit(1))
