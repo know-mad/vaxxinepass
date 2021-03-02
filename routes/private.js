32705de1a6cb9e2d021f0d0006a0c176e32b5073
David_Velez <velezdavid6@gmail.com>
2021-03-02 15:57:33 -0500

    troubleshooting get route

--- routes/private.js
+++ routes/private.js
@@ -4,6 +4,10 @@ const router = express.Router()
 const { protect } = require('../middleware/auth')
 const { getPrivateData } = require('../controllers/private')
 
-router.route('/').get(protect, getPrivateData)
+// router.route('/').get(protect, getPrivateData)
+
+router.get('/', (req, res) => {
+    res.send('Hello World')
+})
 
 module.exports = router
